20040ee7e2fc905388d537eda10d07e8b318067d
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-17 17:04:52 +0200

    [LIOR_9] Injection d'argument à l'autowiring
    Si je passes un argument dans le constructeur qui n'est ps auto-injecté comment je gère l'erreur ? Il faut le définir dans le services.yaml

--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -2,8 +2,19 @@
 
 namespace App\Taxes;
 
+use Psr\Log\LoggerInterface;
+
 class Calculator
 {
+    protected $logger;
+    protected $tva;
+
+    public function __construct(LoggerInterface $logger, float $tva)
+    {
+        $this->logger = $logger;
+        $this->tva = $tva;
+    }
+
     public function calcul(float $prix): float
     {
         return $prix * (20/100);
