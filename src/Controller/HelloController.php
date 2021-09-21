7d1b80136f3d2c8e769bf11cab85170bc2233181
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-21 14:43:30 +0200

    [LIOR_18] Création du render pour rendre générique la réponse
    Non compatible avec AbstractController puisqu'il existe déjà

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -9,25 +9,27 @@ use Twig\Environment;
 
 class HelloController extends AbstractController
 {
+    protected $twig;
+
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
 
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
     public function hello($prenom, Environment $twig)
     {
-        $html = $twig->render('hello.html.twig', [
-            'prenom' => $prenom,
-            'formateur1' => [
-                'prenom' => 'Lior',
-                'nom' => 'Chamla',
-                'age' => 33
-            ],
-            'formateur2' => [
-                'prenom' => 'Mickaël',
-                'nom' => 'Auger',
-                'age' => 43
-            ]
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom
         ]);
+    }
+
+    protected function render($path, $variables = [])
+    {
+        $html = $this->twig->render($path, $variables);
 
         return new Response($html);
     }
