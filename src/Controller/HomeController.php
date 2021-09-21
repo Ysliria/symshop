389e63db4c3cf71bb63007ba86dc93a692ed2639
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-21 16:57:42 +0200

    [LIOR_24][Exercice] Ajouter une entité categorie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,37 +10,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(EntityManagerInterface $em)
+    public function homepage()
     {
-        $productRepository = $em->getRepository(Product::class);
-        $product = new Product;
-
-        /*
-        $product->setName('Table en métal');
-        $product->setPrice(3000);
-        $product->setSlug('table-en-metal');
-        */
-        
-        // Grace au design pattern fluent qui incite à faire un retour de $this, on peut chainer les méthodes.
-        $product
-            ->setName('Table en métal')
-            ->setPrice(3000)
-            ->setSlug('table-en-metal');
-
-        $em->persist($product);
-        $em->flush();
-
-        dd($product);
-
-        // pour mettre a jour
-        $product = $productRepository->find(3);
-        $product->setPrice(2500);
-        $em->flush(); // pas besoin de persister il est déjà en base
-
-        // pour supprimer
-        $em->remove($product);
-        $em->flush();
-
         return $this->render('home.html.twig');
     }
 }
