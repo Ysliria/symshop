b2c7ff6ed32b2cda951c14beeb2f45a5d6951069
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-21 14:48:59 +0200

    [LIOR_19] Utiliser l'abstract controller

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,21 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Twig\Environment;
 
 class HelloController extends AbstractController
 {
-    protected $twig;
-
-
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
@@ -25,12 +16,5 @@ class HelloController extends AbstractController
         return $this->render('hello.html.twig', [
             'prenom' => $prenom
         ]);
-    }
-
-    protected function render($path, $variables = [])
-    {
-        $html = $this->twig->render($path, $variables);
-
-        return new Response($html);
     }
 }
