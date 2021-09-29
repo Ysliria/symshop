a7076d0769fedb2ff27d25e513dfb99e67fabd21
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-29 08:17:21 +0200

    [LIOR_27] Relation et amélioration des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Faker\Factory;
 use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
@@ -23,15 +24,26 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
-        
-        for ($p = 0; $p < 100; $p++) {
-            $product = new Product;
-            $product->setName($faker->productName)
-                ->setPrice($faker->price(4000, 2000))
-                ->setSlug(strtolower($this->slugger->slug($product->getName())));
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
 
-            $manager->persist($product);
+            $category->setName($faker->department)
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+        
+            $manager->persist($category);
+
+            for ($p = 0, $pmax = mt_rand(15, 20); $p < $pmax; $p++) {
+                $product = new Product;
+            
+                $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 2000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category);
+    
+                $manager->persist($product);
+            }
         }
+        
 
         $manager->flush();
     }
