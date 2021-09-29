ff390ec5848b641454e9a07abbc1c195e7063566
Mickaël AUGER <auger.mickael37@gmail.com>
2021-09-29 08:01:24 +0200

    [LIOR_26] Sluggify

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,9 +6,17 @@ use Faker\Factory;
 use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
@@ -20,7 +28,7 @@ class AppFixtures extends Fixture
             $product = new Product;
             $product->setName($faker->productName)
                 ->setPrice($faker->price(4000, 2000))
-                ->setSlug($faker->slug());
+                ->setSlug(strtolower($this->slugger->slug($product->getName())));
 
             $manager->persist($product);
         }
