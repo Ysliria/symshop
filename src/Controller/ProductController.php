4bd61982bd1a20337a98f397f8108564499d8a1a
Mickaël AUGER <auger.mickael37@gmail.com>
2021-10-01 08:51:41 +0200

    [LIOR_34] Amélioration du formulaire
    Type text, textarea, money, choices

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,10 @@ use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -54,10 +58,38 @@ class ProductController extends AbstractController
     public function create(FormFactoryInterface $formFactroyInterface)
     {
         $builder = $formFactroyInterface->createBuilder();
-        $builder->add('name')
-            ->add('shortDescription')
-            ->add('price')
-            ->add('category');
+        $builder->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Saisir le nom du produit'
+            ]
+        ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Saisir une description assez courte mais parlante pour le visiteur'
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Saisir le prix du produit en euro'
+                ]
+            ])
+            ->add('category', ChoiceType::class, [
+                'label' => 'Categorie',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'placeholder' => '-- Choisir une catégorie --',
+                'choices' => [
+                    'Catégorie 1' => 1,
+                    'Categorie 2' => 2
+                ]
+            ]);
 
         $form     = $builder->getForm();
         $formView = $form->createView();
