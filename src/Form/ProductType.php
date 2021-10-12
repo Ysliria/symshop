2e08e501d62c3aff3b6efb3dd2c3088790251a3b
Mickaël AUGER <auger.mickael37@gmail.com>
2021-10-12 09:08:08 +0200

    [LIOR_47][Exemple] EventListener sur les formulaires
    On souhaite permettre le choix de la catégorie que pour la création d'un produit.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,16 +2,18 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
 use App\Entity\Product;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Category;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
 
 class ProductType extends AbstractType
 {
@@ -42,13 +44,23 @@ class ProductType extends AbstractType
                     'placeholder' => 'Tapez une URL d\'image'
                 ]
             ])
-            ->add('category', EntityType::class, [
-                'label' => 'Categorie',
-                'placeholder' => '-- Choisir une catégorie --',
-                'class' => Category::class,
-                'choice_label' => 'name'            
-            ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+            $form = $event->getForm();
+
+            /** @var Product */
+            $product = $event->getData();
+
+            if($product->getId() === null) {
+                $form->add('category', EntityType::class, [
+                    'label' => 'Categorie',
+                    'placeholder' => '-- Choisir une catégorie --',
+                    'class' => Category::class,
+                    'choice_label' => 'name'            
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
