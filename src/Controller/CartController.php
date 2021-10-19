15ec3ffde7bb22cc2203c5f8a07bcfabeff06767
Mickaël AUGER <auger.mickael37@gmail.com>
2021-10-19 14:34:59 +0200

    [LIOR_79] Création du panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id": "\d+"})
+     */
+    public function add($id, Request $request, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw new NotFoundHttpException("Le produit $id n'existe pas !");
+        }
+
+        $cart = $request->getSession()->get('cart', []);
+
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $request->getSession()->set('cart', $cart);
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
+    }
+}
