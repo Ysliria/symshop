270bced00dc03f7c8c2305589fae6197d8dd3fc5
Mickaël AUGER <auger.mickael37@gmail.com>
2021-12-06 12:25:34 +0100

    [LIOR_102] Installation de Stripe et de l'intention de paiement

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Purchase;
 
+use App\Repository\PurchaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,9 +11,27 @@ class PurchasePaymentController extends AbstractController
     /**
      * @Route("/purchase/pay/{id}", name="purchase_payment_form")
      */
-    public function showCardForm()
+    public function showCardForm($id, PurchaseRepository $purchaseRepository)
     {
-        return $this->render('purchase/payment.html.twig');
+        $purchase = $purchaseRepository->find($id);
+
+        if (!$purchase) {
+            return $this->redirectToRoute('cart_show');
+        }
+
+        \Stripe\Stripe::setApiKey(
+            'sk_test_51I5nGIHP4MlDkgKUfdWRVoEw6N6yzkSIlG9XOquCe5yGQFH24wPpdAlxWUJRuiEpczwNKZHdgEKwNM1uNAjof9nU00MXn9eRgS'
+        );
+        $paymentIntent = \Stripe\PaymentIntent::create(
+            [
+                'amount' => $purchase->getTotal(),
+                'currency' => 'eur'
+            ]
+        );
+
+        return $this->render('purchase/payment.html.twig', [
+            'clientSecret' => $paymentIntent->client_secret
+        ]);
     }
 
 }
