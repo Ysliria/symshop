ee6581b137c028eb73116060782683640dc3afa1
Mickaël AUGER <auger.mickael37@gmail.com>
2021-12-12 17:01:40 +0100

    [LIOR_118] Envoi du premier email

--- src/EventDispatcher/ProductViewEmailSubscriber.php
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -5,14 +5,19 @@ namespace App\EventDispatcher;
 use App\Event\ProductViewEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class ProductViewEmailSubscriber implements EventSubscriberInterface
 {
     protected LoggerInterface $logger;
+    protected MailerInterface $mailer;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     public static function getSubscribedEvents(): array
@@ -22,8 +27,21 @@ class ProductViewEmailSubscriber implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
     public function sendViewEmail(ProductViewEvent $productViewEvent)
     {
+        $email = new Email();
+        $email
+            ->from(new Address('contact@mail.com', 'Infos de la boutique'))
+            ->to('admin@mail.com')
+            ->text('Un visiteur est en train de regarder le produit n° ' . $productViewEvent->getProduct()->getid())
+            ->html("<h1>Visite du produit {$productViewEvent->getProduct()->getid()}</h1>")
+            ->subject('Visite du produit n° ' . $productViewEvent->getProduct()->getid())
+        ;
+        $this->mailer->send($email);
+
         $this->logger->info('Le produit ' . $productViewEvent->getProduct()->getName() . ' a été vu !');
     }
 }
